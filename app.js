29935f9769e6fd5b9bae7cf86dd25463c4a5c874
ender <ender>
2017-01-06 23:28:47 +0100

    finished assignment

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 (function(){
   'use strict';
 
-  angular.module('assmnt1App',[])
-  .controller('Assmnt1Controller', function($scope){
-    $scope.feedback="Do it";
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', function($scope){
+    $scope.feedback="";
     $scope.lunchMenu ="";
+    $scope.color="";
     $scope.saySomething = function(){
-      console.log("aa "+ $scope.lunchMenu);
-    }
+      var lunchItems = $scope.lunchMenu.split(",");
+      var itemSize = 0;
+      for(var item in lunchItems){
+        if(!isEmpty(lunchItems[item]))
+        {
+          itemSize++;
+        }
+      }
+
+      if(itemSize == 0){
+        $scope.feedback = "Please enter data first!";
+        $scope.color="red";
+      }
+      else if(itemSize < 4){
+         $scope.feedback = "Enjoy!";
+         $scope.color="green";
+      }
+      else if(itemSize > 3){
+        $scope.feedback = "Too much!";
+        $scope.color="green";
+      }
+
+      function isEmpty(str) {
+          return (typeof str == 'undefined' || !str || str.trim().length === 0);
+      }
+    };
+
+
+
   });
 })();
